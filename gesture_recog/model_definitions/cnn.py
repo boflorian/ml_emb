e323473a649985800c6bc5530d7d82ba041373b5
florianbohnes <florian_bohnes@gmx.de>
2025-11-24 18:31:51 +0200

    adapt cnn architecture from onedcnn learnings. Use as new consolidated standard going forward, disregard other architectures

--- gesture_recog/model_definitions/cnn.py
+++ gesture_recog/model_definitions/cnn.py
@@ -7,39 +7,18 @@ def build_cnn(win=64, num_classes=4, lr=1e-3, l2=None, dropout=None):
     dp = float(dropout) if dropout is not None else 0.0          # LSTM/Dropout need a float
     reg = regularizers.l2(l2) if (l2 is not None and l2 > 0) else None
 
-    inp = keras.Input(shape=(win, 3))
+    inputs = keras.Input(shape=(win, 3))
 
-    # Conv block 1
-    x = layers.Conv1D(
-      32, # filters
-      5, # kernel_size
-      padding="same", # output length = input length
-      activation="relu", # activation function 
-      kernel_regularizer=reg
-    )(inp)
-    x = layers.SpatialDropout1D(0.2)(x)
-    x = layers.MaxPooling1D(2)(x)
+    x = layers.Conv1D(64, kernel_size=9, activation='relu', padding='same')(inputs)
+    x = layers.Conv1D(64, kernel_size=5, activation='relu', padding='same')(x)
+    x = layers.MaxPooling1D(pool_size=2, strides=2)(x)
+    x = layers.Conv1D(64, kernel_size=3, activation='relu', padding='same')(x)
+    x = layers.MaxPooling1D(pool_size=2, strides=2)(x)
 
-    # Conv block 2
-    x = layers.Conv1D(64, 5, padding="same", activation="relu",
-                      kernel_regularizer=reg)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.SpatialDropout1D(0.2)(x)
-    x = layers.MaxPooling1D(2)(x)
-
-    """
-    # Conv block 3
-    x = layers.Conv1D(128, 5, padding="same", activation="relu",
-                      kernel_regularizer=reg)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.MaxPooling1D(2)(x)
-    """
-    
     # Temporal block
     x = layers.Bidirectional(layers.LSTM(96, dropout=dp, return_sequences=False))(x)
 
-    # Head
-    x = layers.Dense(128, activation="relu", kernel_regularizer=reg)(x)
+    x = layers.Dense(256, activation='relu', kernel_regularizer=reg)(x)
     if dp > 0:
         x = layers.Dropout(dp)(x)
     out = layers.Dense(num_classes, activation="softmax")(x)
