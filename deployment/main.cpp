aedff1f660406576dbf2c49304571fb7f7999f13
florianbohnes <florian_bohnes@gmx.de>
2025-11-27 19:12:20 +0000

    imu read

--- deployment/main.cpp
+++ deployment/main.cpp
@@ -18,6 +18,9 @@
 #include "model.h"
 #include "model_settings.h"
 
+// For IMU
+#include "icm20948.h"
+
 using namespace std; 
 #define HALT_CORE_1() while (1) { tight_loop_contents(); }
 
@@ -59,6 +62,15 @@ int main(void)
 
     sleep_ms(5000);
 
+    // Initialize IMU
+    IMU_EN_SENSOR_TYPE imu_type;
+    imuInit(&imu_type);
+    if (imu_type != IMU_EN_SENSOR_TYPE_ICM20948) {
+        printf("IMU not detected\n");
+        HALT_CORE_1();
+    }
+    printf("IMU initialized\n");
+
     // initialize LCD display
     LCD_SCAN_DIR  lcd_scan_dir = SCAN_DIR_DFT;
     LCD_Init(lcd_scan_dir,1000);
@@ -92,47 +104,19 @@ int main(void)
     }
 
     while (true) {
-        // Block the process until data being filled
-        uint32_t g = multicore_fifo_pop_blocking();
+        // Read IMU data
+        IMU_ST_SENSOR_DATA gyro, accel;
+        imuDataAccGyrGet(&gyro, &accel);
 
-        // Acquire the mutex (blocking)
-        mutex_enter_blocking(&mutex);  
+        // Print or process the data
+        printf("Accel: X=%d, Y=%d, Z=%d | Gyro: X=%d, Y=%d, Z=%d\n",
+               accel.s16X, accel.s16Y, accel.s16Z,
+               gyro.s16X, gyro.s16Y, gyro.s16Z);
 
-        inference.IsProcessing = true;
+        // For gesture recognition, collect data and run inference here
+        // e.g., fill a buffer and predict when ready
 
-        // Run inference and set predicted result
-        // This for loop is used for debugging purpose.
-        for (int index=0; index<DIGIT_INPUT_COUNT; index++) {
-          for (int i=0; i<INPUT_IMAGE_SIZE; i++) {
-              for (int j=0; j<INPUT_IMAGE_SIZE; j++) {
-                  uint8_t num = inference.UserInputs[index].InputData[i*INPUT_IMAGE_SIZE + j];
-                  int space = 3 - count_digits(num);
-                  printf("%d", num);
-                  for (int i = 0; i < space; ++i) {
-                      printf(" ");
-                  }
-              }
-              printf("\n");
-          }
-
-          memcpy(test_image_input, inference.UserInputs[index].InputData, byte_size);
-          int result = ml_model.predict();
-          if (result == -1) {
-              printf("Failed to run inference\n");
-              inference.UserInputs[index].PredictedDigit = UNKNOWN_PREDICTION;
-          } else {
-              printf("Predicted: %d\n", result);
-              inference.UserInputs[index].PredictedDigit = result;
-          }
-          sleep_ms(200);
-        }
-
-        printf("Login process finished.\n");
-
-        inference.IsProcessing = false;
-
-        //Return the resource
-        mutex_exit(&mutex);
+        sleep_ms(100); // Adjust sampling rate
     }
     return 0;
 }
