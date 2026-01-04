1b4db894269fc5da519de23324048d49cc1d47cc
florianbohnes <florian_bohnes@gmx.de>
2026-01-04 11:24:00 +0100

    RAM reduce

--- deployment/lib/http_client/lwipopts.h
+++ deployment/lib/http_client/lwipopts.h
@@ -22,10 +22,10 @@
 #endif
 #define MEM_ALIGNMENT               4
 #ifndef MEM_SIZE
-#define MEM_SIZE                    40000
+#define MEM_SIZE                    16000
 #endif
 #ifndef MEMP_NUM_SYS_TIMEOUT
-#define MEMP_NUM_SYS_TIMEOUT        64
+#define MEMP_NUM_SYS_TIMEOUT        16
 #endif
 #ifndef LWIP_TIMERS
 #define LWIP_TIMERS                 1
@@ -33,24 +33,24 @@
 #ifndef SYS_LIGHTWEIGHT_PROT
 #define SYS_LIGHTWEIGHT_PROT        1
 #endif
-#define MEMP_NUM_TCP_SEG            64
-#define MEMP_NUM_ARP_QUEUE          10
-#define PBUF_POOL_SIZE              48
+#define MEMP_NUM_TCP_SEG            16
+#define MEMP_NUM_ARP_QUEUE          4
+#define PBUF_POOL_SIZE              16
 #define LWIP_ARP                    1
 #define LWIP_ETHERNET               1
 #define LWIP_ICMP                   1
 #define LWIP_RAW                    1
-#define TCP_WND                     (8 * TCP_MSS)
+#define TCP_WND                     (2 * TCP_MSS)
 #define TCP_MSS                     1460
-#define TCP_SND_BUF                 (8 * TCP_MSS)
+#define TCP_SND_BUF                 (2 * TCP_MSS)
 #define TCP_SND_QUEUELEN            ((4 * (TCP_SND_BUF) + (TCP_MSS - 1)) / (TCP_MSS))
 #define LWIP_NETIF_STATUS_CALLBACK  1
 #define LWIP_NETIF_LINK_CALLBACK    1
 #define LWIP_NETIF_HOSTNAME         1
 #define LWIP_NETCONN                0
-#define MEM_STATS                   1
-#define SYS_STATS                   1
-#define MEMP_STATS                  1
+#define MEM_STATS                   0
+#define SYS_STATS                   0
+#define MEMP_STATS                  0
 #define LINK_STATS                  0
 // #define ETH_PAD_SIZE                2
 #define LWIP_CHKSUM_ALGORITHM       3
@@ -59,8 +59,8 @@
 #define LWIP_TCP                    1
 #define LWIP_UDP                    1
 #define LWIP_DNS                    1
-#define LWIP_SNTP                   1
-#define SNTP_SERVER_DNS             1
+#define LWIP_SNTP                   0
+#define SNTP_SERVER_DNS             0
 #define LWIP_TCP_KEEPALIVE          1
 #define LWIP_NETIF_TX_SINGLE_PBUF   0
 // Core locking not used with NO_SYS=1
@@ -73,43 +73,8 @@
 #define LWIP_ALTCP_TLS_MBEDTLS      0
 
 
-#ifndef NDEBUG
-#define LWIP_DEBUG                  1
-#define LWIP_STATS                  1
-#define LWIP_STATS_DISPLAY          1
-#endif
-
-#define ETHARP_DEBUG                LWIP_DBG_OFF
-#define NETIF_DEBUG                 LWIP_DBG_OFF
-#define PBUF_DEBUG                  LWIP_DBG_ON
-#define API_LIB_DEBUG               LWIP_DBG_OFF
-#define API_MSG_DEBUG               LWIP_DBG_OFF
-#define SOCKETS_DEBUG               LWIP_DBG_OFF
-#define ICMP_DEBUG                  LWIP_DBG_OFF
-#define INET_DEBUG                  LWIP_DBG_OFF
-#define IP_DEBUG                    LWIP_DBG_OFF
-#define IP_REASS_DEBUG              LWIP_DBG_OFF
-#define RAW_DEBUG                   LWIP_DBG_OFF
-#define MEM_DEBUG                   LWIP_DBG_ON
-#define MEMP_DEBUG                  LWIP_DBG_ON
-#define SYS_DEBUG                   LWIP_DBG_OFF
-#define TCP_DEBUG                   LWIP_DBG_ON
-#define TCP_INPUT_DEBUG             LWIP_DBG_ON
-#define TCP_OUTPUT_DEBUG            LWIP_DBG_ON
-#define TCP_RTO_DEBUG               LWIP_DBG_ON
-#define TCP_CWND_DEBUG              LWIP_DBG_ON
-#define TCP_WND_DEBUG               LWIP_DBG_ON
-#define TCP_FR_DEBUG                LWIP_DBG_ON
-#define TCP_QLEN_DEBUG              LWIP_DBG_ON
-#define TCP_RST_DEBUG               LWIP_DBG_ON
-#define UDP_DEBUG                   LWIP_DBG_OFF
-#define TCPIP_DEBUG                 LWIP_DBG_OFF
-#define DNS_DEBUG                   LWIP_DBG_ON
-#define HTTPC_DEBUG                 LWIP_DBG_ON
-#define ALTCP_DEBUG                 LWIP_DBG_ON
-#define ALTCP_TLS_DEBUG             LWIP_DBG_ON
-#define PPP_DEBUG                   LWIP_DBG_OFF
-#define SLIP_DEBUG                  LWIP_DBG_OFF
-#define DHCP_DEBUG                  LWIP_DBG_OFF
+#define LWIP_DEBUG                  0
+#define LWIP_STATS                  0
+#define LWIP_STATS_DISPLAY          0
 
 #endif /* __LWIPOPTS_H__ */
