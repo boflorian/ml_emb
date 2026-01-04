1aa0b92d42e54b86aad2bce4a6563f1c4081ca5b
florianbohnes <florian_bohnes@gmx.de>
2026-01-04 11:57:46 +0100

    buffer reductions

--- deployment/lib/http_client/lwipopts.h
+++ deployment/lib/http_client/lwipopts.h
@@ -43,8 +43,9 @@
 #define LWIP_RAW                    0
 #define TCP_MSS                     536
 #define TCP_WND                     (2 * TCP_MSS)
-#define TCP_SND_BUF                 (2 * TCP_MSS)
-#define TCP_SND_QUEUELEN            ((4 * (TCP_SND_BUF) + (TCP_MSS - 1)) / (TCP_MSS))
+#define TCP_SND_BUF                 (1 * TCP_MSS)
+#define TCP_SND_QUEUELEN            8
+#define LWIP_DISABLE_TCP_SANITY_CHECKS 1
 #define LWIP_NETIF_STATUS_CALLBACK  0
 #define LWIP_NETIF_LINK_CALLBACK    0
 #define LWIP_NETIF_HOSTNAME         0
